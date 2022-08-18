8145efcccdd0e50f1339777b46a2410803416b15
Siddharth Singh <siddharth.s1@outlook.com>
2022-08-18 19:09:53 +1000

    Modified app.js to include functionality for POSTMAN for PATCH and DELETE

--- week-14/2-wed/nodeAppMirrorFlask/app.js
+++ week-14/2-wed/nodeAppMirrorFlask/app.js
@@ -36,6 +36,38 @@ app.post('/messages', (request, response) => {
     response.json(newMessage);
 });
 
+app.patch('/messages/:id', (request, response) => {
+    const toPatchId = request.params.id;
+    const message = request.body.message;
+    const alterMessage = (dict) => {
+        if (dict.id === toPatchId) {
+            dict.message = message;
+        }
+        return dict;
+    };
+    messages.map(alterMessage);
+    response.json(messages);
+});
+
+app.delete('/messages/:id', (request, response) => {
+    const toDeleteId = request.params.id;
+
+    // was trying below method; it works but only when i copy the filtered array into a new array. decided that it was a less efficient solution than below so i skipped it
+    // const modifyArray = (dict) => {
+    //     return dict.id !== toDeleteId;
+    // };
+    // const newArr = messages.filter(modifyArray);
+    // response.json(newArr);
+
+    for (const message of messages) {
+        if (message.id === toDeleteId) {
+            const index = messages.indexOf(message);
+            messages.splice(index, 1);
+        }
+    }
+    response.json(messages);
+});
+
 app.listen(3000, () => {
     console.log('App listening on http://127.0.0.1:3000');
 }); // similar to python/flask app.run
