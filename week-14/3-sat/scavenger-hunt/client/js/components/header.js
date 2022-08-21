c1c80dcb99e9e96b2ca243563f9c8919e75c7ee0
Siddharth Singh <siddharth.s1@outlook.com>
2022-08-21 12:28:07 +1000

    Added CSS to header

--- week-14/3-sat/scavenger-hunt/client/js/components/header.js
+++ week-14/3-sat/scavenger-hunt/client/js/components/header.js
@@ -3,8 +3,8 @@ const renderHeader = () => {
     header.innerHTML = `
     <h1>Scavenger Hunt</h1>
     <ul id="navlist">
-        <li onClick="renderChallenges()">Challenges</li>
-        <li onClick="renderRules()">Rules</li>
+        <li onClick="renderChallenges()"><button class="nav-btn">Challenges</button></li>
+        <li onClick="renderRules()"><button class="nav-btn">Rules</button></li>
     </ul>
     `;
 };
