a13600e83591d09396e338d3c2b1d1c298e03081
Siddharth Singh <siddharth.s1@outlook.com>
2022-08-21 10:38:38 +1000

    Modified so that api is called only once - if challenges are loaded, api not called again

--- week-14/3-sat/scavenger-hunt/client/js/components/challenges.js
+++ week-14/3-sat/scavenger-hunt/client/js/components/challenges.js
@@ -1,7 +1,18 @@
+const challenges = document.getElementById('challenges');
+
 const renderChallenges = () => {
-    section.innerHTML = '';
+    rules.style.display = 'none';
+    if (challenges.childElementCount === 0) {
+        callApi();
+    }
+    challenges.style.display = 'block';
+};
+
+const callApi = () => {
     axios('/api/challenges')
         .then(function (response) {
+            console.log('api called');
+            apiCalled = true;
             for (const challenge of response.data) {
                 const div = document.createElement('div');
                 div.classList.add('challenge-box');
@@ -12,8 +23,6 @@ const renderChallenges = () => {
                 name.textContent = challenge.name;
                 name.classList.add('challenge-btn');
                 const challengeId = challenge.id;
-                // description.textContent = challenge.description;
-                // address.textContent = challenge.address;
                 div.appendChild(name);
                 let clickState = false;
 
@@ -23,13 +32,18 @@ const renderChallenges = () => {
                         if (description.textContent.length == 0) {
                             axios(`/api/challenges/${challengeId}`)
                                 .then(function (result) {
+                                    console.log('api called - part 2');
                                     description.textContent = result.data[0].description;
                                     address.textContent = result.data[0].address;
+                                    description.classList.add('challenge-details');
+                                    address.classList.add('challenge-details');
                                     div.appendChild(description);
                                     div.appendChild(address);
                                 })
                         }
                         else {
+                            description.classList.add('challenge-details');
+                            address.classList.add('challenge-details');
                             div.appendChild(description);
                             div.appendChild(address);
                         }
@@ -40,8 +54,8 @@ const renderChallenges = () => {
                         div.removeChild(address);
                     }
                 });
-                section.appendChild(div);
+                challenges.appendChild(div);
             }
         })
-        .catch(err => section.innerHTML = '<h2>Error retrieving data!</h2>')
+        .catch(err => challenges.innerHTML = '<h2>Error retrieving data!</h2>')
 };
--- week-14/3-sat/scavenger-hunt/client/js/components/rules.js
+++ week-14/3-sat/scavenger-hunt/client/js/components/rules.js
@@ -1,8 +1,10 @@
-const section = document.getElementById('page');
+const rules = document.getElementById('rules');
 
 const renderRules = () => {
-    section.innerHTML = `
+    challenges.style.display = 'none';
+    rules.innerHTML = `
     <h3>Rules</h3>
     <p>Rule 1: Don't talk about scavenger hunt</p>
     `;
+    rules.style.display = 'block';
 };
